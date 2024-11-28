d69ea3a8db4d9d8cc941f216f7021a8e740a75d3
Maciej-entr <m.waszynski2005@wp.pl>
2024-11-28 23:30:28 +0100

    all

---
+++ src/assets/assets.js
@@ -0,0 +1,33 @@
+import history_icon from './history-icon.png'
+import menu_icon from './menu-icon.png'
+import plus_icon from './plus-icon.png'
+import question_icon from './question-icon.png'
+import setting_icon from './setting-icon.png'
+import bulb_icon from './light-bulb.png'
+import compass_icon from './compass-icon.png'
+import gallery_icon from './gallery-icon.png'
+import mic_icon from './mic-icon.png'
+import user_icon from './user-icon.png'
+import message_icon from './message-icon.png'
+import code_icon from './code-icon.png'
+import send_icon from './send-icon.png'
+import gemini_icon from './gemini-icon.png'
+
+
+export const assets = {
+   history_icon,
+   menu_icon,
+   plus_icon,
+   question_icon,
+   setting_icon,
+   bulb_icon,
+   compass_icon,
+   gallery_icon,
+   mic_icon,
+   user_icon,
+   message_icon,
+   code_icon,
+   send_icon,
+   gemini_icon,
+
+}
---
+++ src/config/gemini.js
@@ -0,0 +1,31 @@
+import {
+    GoogleGenerativeAI,
+  } from "@google/generative-ai";
+  
+  const apiKey ='AIzaSyCv1pe0leZ1t1HwHEmUddKQJeJ18uYC1Tg';
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.0-pro",
+  });
+  
+  const generationConfig = {
+    temperature: 0.9,
+    topP: 1,
+    maxOutputTokens: 2048,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return (result.response.text());
+  }
+  
+  export default run
